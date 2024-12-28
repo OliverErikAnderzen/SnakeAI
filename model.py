ca7f38df803cbc78368ea72de8dcf5f551314a83
StarkStark1 <oliveranderzen@gmail.com>
2024-12-28 23:23:09 +0100

    Create model.py

---
+++ model.py
@@ -0,0 +1,13 @@
+class SnakeGameModel:
+    def __init__(self, grid_size):
+        self.grid_size = grid_size
+        self.reset()
+
+    def reset(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def get_state(self):
+        pass
