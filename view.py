89f49847e963848833cf4255316ec96ad452d3a4
StarkStark1 <oliveranderzen@gmail.com>
2024-12-28 23:55:38 +0100

    Implement pygame

--- view.py
+++ view.py
@@ -1,8 +1,26 @@
+import pygame
+
 class SnakeGameView:
     def __init__(self, model):
+        pygame.init()
         self.model = model
+        self.grid_size = model.grid_size
+        self.cell_size = 20
+        self.screen = pygame.display.set_mode(
+            (self.grid_size[0] * self.cell_size, self.grid_size[1] * self.cell_size) 
+        )
+        pygame.display.set_caption("SnakeAI")
 
     def render(self):
-        pass
+        self.screen.fill((0, 0, 0))
 
-    
+        for segment in self.model.snake:
+            x, y = segment
+            pygame.draw.rect(self.screen, (0, 255, 0),
+                             (x * self.cell_size, y*self.cell_size, self.cell_size, self.cell_size))
+            
+        fx, fy = self.model.food
+        pygame.draw.rect(self.screen, (255, 0, 0),
+                         (fx * self.cell_size, fy * self.cell_size, self.cell_size, self.cell_size))
+
+        pygame.display.flip()
