633875fbb64bbee640839a720a91a24c020d902f
StarkStark1 <oliveranderzen@gmail.com>
2024-12-28 23:55:12 +0100

    Handle Input in Controller.py

--- controller.py
+++ controller.py
@@ -1,3 +1,29 @@
+import time
+import pygame
+
 class SnakeGameController:
-    def __init__(self, model):
+    def __init__(self, model, view):
         self.model = model
+        self.view = view
+
+    def get_player_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.model.game_over = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    return "UP"
+                elif event.key == pygame.K_DOWN:
+                    return "DOWN"
+                elif event.key == pygame.K_LEFT:
+                    return "LEFT"
+                elif event.key == pygame.K_RIGHT:
+                    return "RIGHT"
+        return None
+
+    def run(self):
+        while not self.model.is_game_over():
+            action = self.get_player_input()
+            if action:
+                self.model.step(action)
+            self.view.render()
