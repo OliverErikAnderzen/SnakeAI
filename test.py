db027459ab3db35fbc454d088746d30ecea5e92b
OliverErikAnderzen <olivererikanderzen@gmail.com>
2025-01-02 23:34:18 +0100

    implement runGame function (TEST)

--- test.py
+++ test.py
@@ -1,14 +1,32 @@
 import pygame
 
-
-
 grid_size = (20, 20)
 cell_size = 20
 
+snake_position = (5, 5)
+
+BLACK = (0,0,0)
+GREEN = (0,255,0)
+RED = (255,0,0)
+
 pygame.init()
 screen = pygame.display.set_mode((grid_size[0]*cell_size, grid_size[1]*cell_size))
-screen.fill((0,0,0))
+pygame.display.set_caption("SnakeAI")
 
-running = True
-while running:
-    for event in pygame.event.get():
+
+def runGame():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                running = False
+            
+            screen.fill((0,0,0))
+
+            
+
+
+
+if __name__ == "__main__":
+    runGame()
